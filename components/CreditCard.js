c787fd2a2386d127c28623cd1c7b8a28d13c31d4
ebharathi <elayabharathi10102002@gmail.com>
2023-11-20 13:51:51 +0530

    added input box designs

--- components/CreditCard.js
+++ components/CreditCard.js
@@ -4,6 +4,7 @@ import 'react-credit-cards/es/styles-compiled.css';
 const CreditCard=()=>{
     const [name,setName]=useState("FALSE-BOTTOM ENCRYPTIION");
     const [number,setNumber]=useState("");
+    const [cardNumber,setCardNumber]=useState([]);
     const [cvc,setCvc]=useState("");
     const [expiry,setExpiry]=useState("");
     const [focused,setFocused]=useState("");
@@ -14,8 +15,48 @@ const CreditCard=()=>{
     let year=[23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40];
     //useEffect for expirydate select
     useEffect(()=>{
-        setExpiry(expiryMonthSelect+expiryYearSelect);
+        setExpiry(expiryMonthSelect+expiryYearSelect.substring(2));
     },[expiryMonthSelect,expiryYearSelect])
+    //handle iput change for number
+    const handleInputChange = (e, index) => {
+        const value = e.target.value;
+        if (/^\d*$/.test(value) && index >= 0 && index <= 3) {
+            setCardNumber(prevArray => {
+                const newArray = [...prevArray]; // Create a copy of the original array
+                newArray[index] = value; // Update the value at the specified index
+                let n=""; 
+                newArray.map((i)=>{
+                      n+=i;
+                 })
+                 console.log("Number ------>",n)
+                 setNumber(n);
+                return newArray;
+              });
+        }
+      };
+      useState(()=>{
+        let n=""; 
+        cardNumber.map((i)=>{
+              n+=i;
+         })
+         setNumber(n);
+      },[cardNumber])
+      function moveToNext(currentInput, nextInputId) {
+        var maxLength = 4;
+        if (currentInput.target.value.length >= maxLength) {
+            var nextInput = document.getElementById(nextInputId);
+
+            if (nextInput) {
+                nextInput.focus();
+            }
+        }
+      }
+      const handleSubmit=(e)=>{
+        e.preventDefault();
+         console.log("CARD NUMBER--->",number);
+         console.log("CVC--->",cvc);
+         console.log("EXPIRY DATE--->",expiry)
+      }
    return(
     <div className='flex justify-center items-center mt-20'>
       <div className='flex flex-col'>
@@ -28,47 +69,61 @@ const CreditCard=()=>{
             number={number}
             />
         </div>
-        <div className='flex justify-center items-center  px-20'>
-            <form className='pt-20'>
-                <input 
-                 className='border-1 border-[#70706f] w-custom-1 px-6 py-3 rounded-lg outline-none '
-                 placeholder='Card Number' 
-                 type='number'
-                 name="number" 
-                 onChange={(e)=>{setNumber(e.target.value);}}
-                 onFocus={(e)=>setFocused(e.target.name)}/>
-                 <br/>
-                 <br/>
-                <input
-                 className='border-1 border-[#70706f] w-custom-1 px-6 py-3 rounded-lg outline-none'
+        <div className='flex justify-center items-center  px-20 '>
+            <form className='pt-10'>
+            <div className='grid grid-cols-4'>
+            {Array.from({ length: 4 }).map((_, index) => (
+                    <input
+                    key={index}
+                    className='border-1 border-[#70706f] w-20 mx-1 px-5 py-2 rounded-md outline-none'
+                    id={`input${index+1}`}
+                    type='text'
+                    name={`number-${index + 1}`}
+                    maxLength={4}
+                    value={cardNumber[index] || ''}
+                    onInput={(e)=>moveToNext(e,`${'input'+(index+2)}`)}
+                    onChange={(e) => handleInputChange(e, index)}
+                    onFocus={(e) => e.target.select()}
+                    />
+            ))}
+
+            </div>
+            <div className='text-center py-3'>
+            <input
+                 className='border-1 border-[#70706f] w-full px-6 py-2 rounded-md outline-none'
                  placeholder='CVC' 
-                 type='number'
+                 type='text'
                  name="cvc" 
+                 value={cvc}
+                 maxLength={3}
                  onChange={(e)=>{setCvc(e.target.value)}}
                  onFocus={(e)=>setFocused(e.target.name)}/>
-                 <br/>
-                 <br/>
+            </div>
+            <div className='text-center'>
                  <select
-                 className='border-1 border-[#70706f] w-40 px-6 py-3 rounded-lg outline-none'
+                 className='border-1 border-[#70706f] w-40 px-6 py-2 rounded-md outline-none'
                  name="expiry"
                  onChange={(e)=>setExpiryMonthSelect(e.target.value)}
                  onFocus={(e)=>setFocused(e.target.name)}
                  >
+                        <option selected disabled>MM</option>
                         {months.map((single)=><option>{single}</option>)}
                  </select>
                  <select
-                 className='mx-3 border-1 border-[#70706f] w-32 px-6 py-3 rounded-lg outline-none'
+                 className='ml-1 border-1 border-[#70706f] w-48 px-6 py-2 rounded-md outline-none'
                  name="expiry"
                  onChange={(e)=>setExpiryYearSelect(e.target.value)}
                  onFocus={(e)=>setFocused(e.target.name)}
                    >
+                       <option selected disabled>YY</option>
                        {year.map((single)=><option>20{single}</option>)}
                  </select>
+            </div>
                  <br/>
                  <br/>
                  <div className='text-center'>
                     <button
-                    type="submit"
+                     onClick={(e)=>handleSubmit(e)}
                     className='btn bg-black  px-5 py-2 rounded-lg text-white w-full hover:text-[black] hover:border-1 hover:border-black hover:bg-white'
                     >
                         PAY
