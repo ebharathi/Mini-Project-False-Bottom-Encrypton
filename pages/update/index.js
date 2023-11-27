40a9496f5e19add806a47918b4046c51f7e4b324
ebharathi <elayabharathi10102002@gmail.com>
2023-11-27 23:41:58 +0530

    finsih

--- pages/update/index.js
+++ pages/update/index.js
@@ -1,16 +1,27 @@
 import Navbar from "@/components/Navbar";
 import UpdateCreditCard from "@/components/UpdateCreditCard";
-import { useState } from "react";
+import { list } from "postcss";
+import { useEffect, useState } from "react";
 const Update = () => {
     const [blur, setIsBlur] = useState(false);
     const [val, setVal] = useState("");
+    const [list1, setList1] = useState("");
+    const [list2, setList2] = useState("");
+    const [list3, setList3] = useState("");
+    
+    const [item1,setItem1]=useState("")
+    const [item2,setItem2]=useState("")
+
     const [selectedOption, setSelectedOption] = useState("0");
+
+    const [success, setSuccess] = useState(false);
     const updateCardInfos = async (name) => {
         setIsBlur(true);
     }
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("value entered-->", val," for-->",selectedOption);
+        console.log("value entered-->", val, " for-->", selectedOption);
+        setSuccess(true);
     }
     return (
         <main className="relative">
@@ -27,16 +38,24 @@ const Update = () => {
                 <div className="relative">
                     <div className="text-center py-10">
                            <select className="px-5 py-2 rounded-lg w-80" onChange={(e)=>setSelectedOption(e.target.value)}>
-                              <option value={"0"}>CARD NUMBER (1st FOUR DIGITS)</option>
-                              <option value={"1"}>CARD NUMBER (2nd FOUR DIGITS)</option>
-                              <option value={"2"}>CARD NUMBER (3rd FOUR DIGITS)</option>
-                              <option value={"3"}>CARD NUMBER (4th FOUR DIGITS)</option>
-                              <option value="cvc">CARD CVC</option>
-                              <option value="expiry">EXPIRY</option>
+                              <option value={0}>CARD NUMBER (1st FOUR DIGITS)</option>
+                              <option value={1}>CARD NUMBER (2nd FOUR DIGITS)</option>
+                              <option value={2}>CARD NUMBER (3rd FOUR DIGITS)</option>
+                              <option value={3}>CARD NUMBER (4th FOUR DIGITS)</option>
+                              <option value={4}>CARD CVC</option>
+                              <option value={5}>EXPIRY</option>
                         </select>
-                        <input value={val} onChange={(e)=>setVal(e.target.value)} className="my-5 px-5 py-2 rounded-lg w-80" maxLength={4} />
-                        <button className="mt-1 mb-3 px-5 py-2 rounded-md text-white bg-black border-1 border-black hover:text-black hover:bg-white" onClick={(e)=>handleSubmit(e)}>
-                             UPDATE MY CARD
+                        <div className="grid grid-cols-3 px-14">
+                                <input value={list1} onChange={(e)=>setList1(e.target.value)} className="my-5 px-5 py-2 rounded-lg w-20" maxLength={1} />
+                                <input value={list2} onChange={(e)=>setList2(e.target.value)} className="my-5 px-5 py-2 rounded-lg w-20" maxLength={1} />
+                                <input value={list3} onChange={(e)=>setList3(e.target.value)} className="my-5 px-5 py-2 rounded-lg w-20" maxLength={1} />
+                        </div>
+                        <div className="grid grid-cols-2 px-20">
+                                <input value={item1} onChange={(e)=>setItem1(e.target.value)} className="my-2 px-5 py-2 rounded-lg w-24" maxLength={1} />
+                                <input value={item2} onChange={(e)=>setItem2(e.target.value)} className="my-2 px-5 py-2 rounded-lg w-24" maxLength={1} />
+                        </div>
+                        <button className={`mt-1 mb-3 px-5 py-2 rounded-md text-white bg-black border-1 border-black hover:text-black hover:bg-white ${success?'bg-green-500 text-white':''}`} onClick={(e)=>handleSubmit(e)}>
+                            {success?'SUCCESSFULLY UPDATED':'UPDATE MY CARD'}
                         </button>
                     </div>
                     <span className="absolute top-0 right-3 text-[20px] cursor-pointer text-white" onClick={()=>setIsBlur(false)}>X</span>
